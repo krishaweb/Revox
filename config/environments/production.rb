0a4bf91acc831fb4370448cf1cdd7e20ce96afdd
Nirav <niravp@krishaweb.com>
2015-10-06 12:52:26 +0530

    changesin production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,16 @@ Rails.application.configure do
   #   :domain =>         'heroku.com',
   #   :authentication => :plain
   # }
-  config.action_mailer.delivery_method = :sparkpost
+  # config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'revoxe.herokuapp.com', #eg: 'yourappname.herokuapp.com'
+    :authentication => :plain,
+  }
   config.action_mailer.perform_deliveries    = true
   config.action_mailer.raise_delivery_errors = true
 
