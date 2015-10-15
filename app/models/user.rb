646535497324aa2ecc2e2e996fc9362af5ca6b88
Nirav <niravp@krishaweb.com>
2015-10-15 12:10:57 +0530

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   validates :lname, presence: {:message => "Please enter your last name"}
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
   validates :email, uniqueness: {:message => "This email is already used"}, on: :create
-  validates :image, presence: {:message => "Please select image"}
+  validates :image, presence: {:message => "Please select image"}, on: :create
 
   extend Enumerize
   enumerize :role, in: [:member, :admin], default: :member, predicates: true
